fa7c0ea4a717ecc537cceb942e0125315ad92f9d
Omar Aldayr García Hernández <aldayr935@gmail.com>
2026-09-08 10:36:34 -0600

    og image fix

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,7 @@
 export interface Service {
   id: string;
   icon: string;
-  icon2?: string;   // ← path secundario opcional
+  icon2?: string;   
   title: string;
   desc: string;
   featured?: boolean;
